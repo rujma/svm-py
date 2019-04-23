f8696ce6e7d6cbc7c00c6fb75377614e73be9ca5
Rui <rjmalmeida0@gmail.com>
2019-04-23 18:28:38 +0100

    added file write support

--- svm_abalone.py
+++ svm_abalone.py
@@ -1,4 +1,5 @@
 # Global imports
+import sys
 import numpy as np
 import pandas as pd
 from scipy import stats
@@ -16,6 +17,8 @@ from split_dataset import split_dataset
 from model_accuracy import model_accuracy
 from scale_data import scale_data
 # Ignore warnings
+np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(suppress=True)
 import warnings
 warnings.filterwarnings('ignore')
 # Start dataset analysis
@@ -23,27 +26,27 @@ abalone = pd.read_csv('abalone.csv')
 # Clean data that has 0.0 height and the outliers that may cause skewness
 abalone = abalone[abalone.Height > 0]
 abalone = abalone[abalone.Height < 0.4]
+#abalone = abalone[abalone.Sex != 'I']
 abalone = pd.get_dummies(abalone)
+print('Data shape: ', abalone.shape)
 # Categorize Rings feature into young (0) and adult (1)
 abalone['Rings'] = pd.cut(abalone['Rings'], [0,8,abalone['Rings'].max()], labels = [0,1])
-#print(abalone.head(10))
 # Split dataset for classification
 y = abalone['Rings']
-X = abalone.drop(['Rings', 'Sex_F', 'Sex_I', 'Sex_M'], axis = 1)
+X = abalone.drop(['Shucked weight', 'Viscera weight', 'Shell weight', 'Rings'], axis = 1)
 X = scale_data(X, type_of_scale = 'Standard')
 X_train, X_test, y_train, y_test = split_dataset(X, y, 0.20)
 # Prepare for SVM
 #svmParams = {'kernel':['linear', 'poly', 'rbf'], 'C':[0.1,1,10,100],'gamma':[0.01,0.1,0.5,1,2]}
 #svmModel = GridSearchCV(svm.SVC(), svmParams, cv=5)
-#svmModel = svm.SVC(kernel='rbf', gamma=1, C=100)
-svmModel = svm.SVC(kernel='linear', C=100)
+#svmModel = svm.SVC(kernel='rbf', gamma=1, C=0.1)
+svmModel = svm.SVC(kernel='linear', C=0.1)
 print('Parameters after fit: ')
 print(svmModel.fit(X_train, y_train))
-print('Support vector matrix: ', svmModel.support_vectors_.shape)
-print('Support vectors: ', svmModel.support_vectors_)
-print('Intercept: ', svmModel.intercept_)
 print('Dual coef matrix: ', svmModel.dual_coef_.shape)
-print('Dual Coef: ', svmModel.dual_coef_)
+print('Support vector matrix: ', svmModel.support_vectors_.shape)
+np.savetxt('test.out', np.array(svmModel.support_vectors_), '%5.5f' ,delimiter=',')
+print('Input matrix: ', X_test.shape)
 # Evaluate model accuracy
 cross_score = cross_val_score(svmModel, X_train, y_train, cv=5)
 print('Cross score: ', cross_score)
