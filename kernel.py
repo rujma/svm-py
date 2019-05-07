ec010be543e98b5ae5a47b46da12ed03408f4494
Rui Almeida <rjmalmeida0@gmail.com>
2019-05-07 11:13:00 +0100

    Alternative classification

--- kernel.py
+++ kernel.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
+from decimal import Decimal
 
 def kernel_linear(SV, Alphas, Bias, X_test):
 
@@ -21,7 +22,7 @@ def kernel_poly(SV, Alphas, Bias, Gamma, Degree, Coeff, X_test):
     y_pred_looped = []
     for i in range(0, len(X_test)):
         result = np.dot(SV, X_test[i])
-        result = result / (Gamma**2)
+        result = result * Gamma
         result = (Coeff + result)**Degree
         result = np.dot(Alphas, result)
         result = result + Bias
@@ -39,8 +40,7 @@ def kernel_rbf(SV, Alphas, Bias, Gamma, X_test):
 
     for i in range(0, len(X_test)):
         result = euclidean_distances(SV, X_test[i].reshape(1, -1),  squared=True)
-        result = result / (Gamma**2)
-        result = np.exp(-result)
+        result = np.exp(-Gamma * result)
         result = np.dot(Alphas, result)
         result = result + Bias
         if result > 0:
